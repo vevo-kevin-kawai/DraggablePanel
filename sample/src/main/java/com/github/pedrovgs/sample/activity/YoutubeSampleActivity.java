9d085feadbfccbf4888b51185429801dd9fd68f7
vevo-kevin-kawai <kevin.kawai@vevo.com>
2015-10-26 17:38:42 -0700

    experimental stuff

--- sample/src/main/java/com/github/pedrovgs/sample/activity/YoutubeSampleActivity.java
+++ sample/src/main/java/com/github/pedrovgs/sample/activity/YoutubeSampleActivity.java
@@ -17,10 +17,9 @@ package com.github.pedrovgs.sample.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 import android.widget.ImageView;
-import butterknife.ButterKnife;
-import butterknife.InjectView;
-import butterknife.OnClick;
+
 import com.github.pedrovgs.DraggableListener;
 import com.github.pedrovgs.DraggablePanel;
 import com.github.pedrovgs.sample.R;
@@ -30,6 +29,10 @@ import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 import com.squareup.picasso.Picasso;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
 /**
  * Sample activity created to show a video from YouTube using a YouTubePlayer.
  *
@@ -37,121 +40,131 @@ import com.squareup.picasso.Picasso;
  */
 public class YoutubeSampleActivity extends FragmentActivity {
 
-  private static final String YOUTUBE_API_KEY = "AIzaSyC1rMU-mkhoyTvBIdTnYU0dss0tU9vtK48";
-  private static final String VIDEO_KEY = "gsjtg7m1MMM";
-  private static final String VIDEO_POSTER_THUMBNAIL =
-      "http://4.bp.blogspot.com/-BT6IshdVsoA/UjfnTo_TkBI/AAAAAAAAMWk/JvDCYCoFRlQ/s1600/"
-          + "xmenDOFP.wobbly.1.jpg";
-  private static final String SECOND_VIDEO_POSTER_THUMBNAIL =
-      "http://media.comicbook.com/wp-content/uploads/2013/07/x-men-days-of-future-past"
-          + "-wolverine-poster.jpg";
-  private static final String VIDEO_POSTER_TITLE = "X-Men: Days of Future Past";
+   private static final String YOUTUBE_API_KEY = "AIzaSyC1rMU-mkhoyTvBIdTnYU0dss0tU9vtK48";
+   private static final String VIDEO_KEY = "gsjtg7m1MMM";
+   private static final String VIDEO_POSTER_THUMBNAIL = "http://4.bp.blogspot.com/-BT6IshdVsoA/UjfnTo_TkBI/AAAAAAAAMWk/JvDCYCoFRlQ/s1600/" + "xmenDOFP.wobbly.1.jpg";
+   private static final String SECOND_VIDEO_POSTER_THUMBNAIL = "http://media.comicbook.com/wp-content/uploads/2013/07/x-men-days-of-future-past" + "-wolverine-poster.jpg";
+   private static final String VIDEO_POSTER_TITLE = "X-Men: Days of Future Past";
 
-  @InjectView(R.id.iv_thumbnail) ImageView thumbnailImageView;
-  @InjectView(R.id.draggable_panel) DraggablePanel draggablePanel;
+   @InjectView(R.id.iv_thumbnail)
+   ImageView thumbnailImageView;
+   @InjectView(R.id.draggable_panel)
+   DraggablePanel draggablePanel;
 
-  private YouTubePlayer youtubePlayer;
-  private YouTubePlayerSupportFragment youtubeFragment;
+   private YouTubePlayer youtubePlayer;
+   private YouTubePlayerSupportFragment youtubeFragment;
 
-  /**
-   * Initialize the Activity with some injected data.
-   */
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_youtube_sample);
-    ButterKnife.inject(this);
-    initializeYoutubeFragment();
-    initializeDraggablePanel();
-    hookDraggablePanelListeners();
-  }
+   /**
+    * Initialize the Activity with some injected data.
+    */
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_youtube_sample);
+      ButterKnife.inject(this);
 
-  /**
-   * Method triggered when the iv_thumbnail widget is clicked. This method maximize the
-   * DraggablePanel.
-   */
-  @OnClick(R.id.iv_thumbnail) void onContainerClicked() {
-    draggablePanel.maximize();
-  }
+      findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(final View view) {
+            initializeYoutubeFragment();
+            initializeDraggablePanel();
+            hookDraggablePanelListeners();
+         }
+      });
 
-  /**
-   * Initialize the YouTubeSupportFrament attached as top fragment to the DraggablePanel widget and
-   * reproduce the YouTube video represented with a YouTube url.
-   */
-  private void initializeYoutubeFragment() {
-    youtubeFragment = new YouTubePlayerSupportFragment();
-    youtubeFragment.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+      Picasso.with(this).load(SECOND_VIDEO_POSTER_THUMBNAIL).placeholder(R.drawable.xmen_placeholder).into(thumbnailImageView);
+   }
 
-      @Override public void onInitializationSuccess(YouTubePlayer.Provider provider,
-          YouTubePlayer player, boolean wasRestored) {
-        if (!wasRestored) {
-          youtubePlayer = player;
-          youtubePlayer.loadVideo(VIDEO_KEY);
-          youtubePlayer.setShowFullscreenButton(true);
-        }
+   /**
+    * Method triggered when the iv_thumbnail widget is clicked. This method maximize the
+    * DraggablePanel.
+    */
+   @OnClick(R.id.iv_thumbnail)
+   void onContainerClicked() {
+      //raggablePanel.maximize();
+   }
+
+   /**
+    * Initialize the YouTubeSupportFrament attached as top fragment to the DraggablePanel widget and
+    * reproduce the YouTube video represented with a YouTube url.
+    */
+   private void initializeYoutubeFragment() {
+      youtubeFragment = new YouTubePlayerSupportFragment();
+      youtubeFragment.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+
+         @Override
+         public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+            if (!wasRestored) {
+               youtubePlayer = player;
+               youtubePlayer.loadVideo(VIDEO_KEY);
+               youtubePlayer.setShowFullscreenButton(true);
+            }
+         }
+
+         @Override
+         public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult error) {
+         }
+      });
+   }
+
+   /**
+    * Initialize and configure the DraggablePanel widget with two fragments and some attributes.
+    */
+   private void initializeDraggablePanel() {
+      draggablePanel.setVisibility(View.VISIBLE);
+      draggablePanel.setFragmentManager(getSupportFragmentManager());
+      draggablePanel.setTopFragment(youtubeFragment);
+      MoviePosterFragment moviePosterFragment = new MoviePosterFragment();
+      moviePosterFragment.setPoster(VIDEO_POSTER_THUMBNAIL);
+      moviePosterFragment.setPosterTitle(VIDEO_POSTER_TITLE);
+      draggablePanel.setBottomFragment(moviePosterFragment);
+      draggablePanel.initializeView();
+
+   }
+
+   /**
+    * Hook the DraggableListener to DraggablePanel to pause or resume the video when the
+    * DragglabePanel is maximized or closed.
+    */
+   private void hookDraggablePanelListeners() {
+      draggablePanel.setDraggableListener(new DraggableListener() {
+         @Override
+         public void onMaximized() {
+            playVideo();
+         }
+
+         @Override
+         public void onMinimized() {
+            //Empty
+         }
+
+         @Override
+         public void onClosedToLeft() {
+            pauseVideo();
+         }
+
+         @Override
+         public void onClosedToRight() {
+            pauseVideo();
+         }
+      });
+   }
+
+   /**
+    * Pause the video reproduced in the YouTubePlayer.
+    */
+   private void pauseVideo() {
+      if (youtubePlayer.isPlaying()) {
+         youtubePlayer.pause();
       }
+   }
 
-      @Override public void onInitializationFailure(YouTubePlayer.Provider provider,
-          YouTubeInitializationResult error) {
+   /**
+    * Resume the video reproduced in the YouTubePlayer.
+    */
+   private void playVideo() {
+      if (!youtubePlayer.isPlaying()) {
+         youtubePlayer.play();
       }
-    });
-  }
-
-  /**
-   * Initialize and configure the DraggablePanel widget with two fragments and some attributes.
-   */
-  private void initializeDraggablePanel() {
-    draggablePanel.setFragmentManager(getSupportFragmentManager());
-    draggablePanel.setTopFragment(youtubeFragment);
-    MoviePosterFragment moviePosterFragment = new MoviePosterFragment();
-    moviePosterFragment.setPoster(VIDEO_POSTER_THUMBNAIL);
-    moviePosterFragment.setPosterTitle(VIDEO_POSTER_TITLE);
-    draggablePanel.setBottomFragment(moviePosterFragment);
-    draggablePanel.initializeView();
-    Picasso.with(this)
-        .load(SECOND_VIDEO_POSTER_THUMBNAIL)
-        .placeholder(R.drawable.xmen_placeholder)
-        .into(thumbnailImageView);
-  }
-
-  /**
-   * Hook the DraggableListener to DraggablePanel to pause or resume the video when the
-   * DragglabePanel is maximized or closed.
-   */
-  private void hookDraggablePanelListeners() {
-    draggablePanel.setDraggableListener(new DraggableListener() {
-      @Override public void onMaximized() {
-        playVideo();
-      }
-
-      @Override public void onMinimized() {
-        //Empty
-      }
-
-      @Override public void onClosedToLeft() {
-        pauseVideo();
-      }
-
-      @Override public void onClosedToRight() {
-        pauseVideo();
-      }
-    });
-  }
-
-  /**
-   * Pause the video reproduced in the YouTubePlayer.
-   */
-  private void pauseVideo() {
-    if (youtubePlayer.isPlaying()) {
-      youtubePlayer.pause();
-    }
-  }
-
-  /**
-   * Resume the video reproduced in the YouTubePlayer.
-   */
-  private void playVideo() {
-    if (!youtubePlayer.isPlaying()) {
-      youtubePlayer.play();
-    }
-  }
+   }
 }
